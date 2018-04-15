d285a9c2e6dafa4a3301a16f3008d16ae372ce01
bjzquan <bjzquan@cn.ibm.com>
2018-04-16 03:57:00 +0800

    封装纯文本打印接口

--- lib/memobird.rb
+++ lib/memobird.rb
@@ -1,5 +1,94 @@
-require "memobird/version"
+require 'memobird/version'
+require 'erb'
+require 'json'
+require 'addressable/uri'
+require 'http'
 
+# Memobird API wrapper
 module Memobird
-  # Your code goes here...
+
+  HOST = 'http://open.memobird.cn/home'.freeze
+  API_RETRIEVE_USER_ID = "#{HOST}/setuserbind".freeze
+  API_PRINT_CONTENT = "#{HOST}/printpaper".freeze
+
+  class << self
+    attr_accessor :api_key, :user_id, :memobird_id, :user_identifying
+
+    def print_content(content = {})
+      raise 'Please set up first' if not_ready?
+      load_user_id if user_id.nil?
+      puts content
+      response = http_get(build_url(API_PRINT_CONTENT,
+                                    userid: user_id,
+                                    printcontent: build_print_content(content)))
+      puts response['showapi_res_error'].to_s + ' ' + response['printcontentid'].to_s
+    rescue StandardError => e
+      puts 'Print content error. ' + e.message
+    end
+
+    private
+
+    def not_ready?
+      api_key.nil? || memobird_id.nil? || user_identifying.nil?
+    end
+
+    def http_get(url)
+      puts "Call #{url}"
+      JSON.parse(HTTP.get(url).to_s)
+    end
+
+    def load_user_id
+      File.open('user.id.txt', 'r').each do |line|
+        self.user_id = line.delete("\n")
+        puts 'User id ' + user_id
+      end
+    rescue StandardError => e
+      puts 'Load user id error. ' + e.message
+      puts 'Retrieve user id from API'
+      retrieve_user_id
+      save_user_id
+    end
+
+    def retrieve_user_id
+      response = http_get(build_url(API_RETRIEVE_USER_ID,
+                                    useridentifying: user_identifying))
+      self.user_id = response['showapi_userid'] if call_ok?(response)
+    rescue StandardError => e
+      puts 'Retrieve user id error. ' + e.message
+      self.user_id = nil
+    end
+
+    def save_user_id
+      File.open('user.id.txt', 'w') do |f|
+        f.puts user_id
+      end
+    rescue StandardError => e
+      puts 'Save user id error. ' + e.message
+    end
+
+    # response of json format
+    def call_ok?(response = {})
+      response['showapi_res_code'] == 1 && response['showapi_res_error'] == 'ok'
+    rescue StandardError
+      false
+    end
+
+    def build_url(url_base, params = {})
+      params[:ak] = api_key
+      params[:timestamp] = ERB::Util.url_encode(Time.now.strftime('%F %T'))
+      params[:memobirdid] = memobird_id
+      url = Addressable::URI.new
+      url.query_values = params
+      "#{url_base}?#{url.query}"
+    end
+
+    def build_print_content(content = {})
+      build_text(content[:text]) || build_text(">> NO CONTENT <<\n")
+    end
+
+    def build_text(text)
+      return nil if text.nil?
+      'T:' + Base64.encode64(text.encode('GBK')).delete("\n")
+    end
+  end
 end
--- memobird.gemspec
+++ memobird.gemspec
@@ -1,36 +1,35 @@
-
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "memobird/version"
+require 'memobird/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "memobird"
-  spec.version       = Memobird::VERSION
-  spec.authors       = ["bjzquan"]
-  spec.email         = ["bjzquan@cn.ibm.com"]
+  spec.name = 'memobird'
+  spec.version = Memobird::VERSION
+  spec.authors = ['aaron67']
+  spec.email = ['aaron67@aaron67.cc']
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
-  spec.license       = "MIT"
+  spec.summary = %q{封装咕咕机官方API}
+  spec.description = %q{封装咕咕机官方提供的打印接口}
+  spec.homepage = 'https://github.com/gitzhou/memobird'
+  spec.license = 'MIT'
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+    spec.metadata['allowed_push_host'] = 'https://rubygems.org'
   else
-    raise "RubyGems 2.0 or newer is required to protect against " \
-      "public gem pushes."
+    raise 'RubyGems 2.0 or newer is required to protect against public gem pushes.'
   end
 
-  spec.files         = `git ls-files -z`.split("\x0").reject do |f|
+  spec.files = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
   end
-  spec.bindir        = "exe"
-  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.bindir = 'exe'
+  spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "minitest", "~> 5.0"
+  spec.add_development_dependency 'addressable', '~>2.5.2'
+  spec.add_development_dependency 'bundler', '~> 1.16'
+  spec.add_development_dependency 'http', '~>2.1.0'
+  spec.add_development_dependency 'rake', '~> 10.0'
 end
